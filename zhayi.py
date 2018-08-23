e29d85e1025d59865e799772279dc4cc97887d30
zhaoyi <huashuizhaoyi@163.com>
2018-08-23 17:54:15 +0800

    删除文件

--- zhayi.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-import os
-filename = r"F:\code\SSD-Tensorflow\VOC2007\test\JPEGImages\000001.jpg"
-f=os.path.abspath(filename)
-print(f)
-#image_data = tf.gfile.FastGFile(filename, 'rb').read()
